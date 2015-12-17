b099bd2b79528abfc1d75e0619f23653a97372e5
Ly Nguyen <Ly900@aol.com>
2015-12-17 15:51:50 -0500

    replace song index content with a song show directive

---
+++ js/songs/song.directive.js
@@ -0,0 +1,18 @@
+"use strict";
+
+(function(){
+  angular
+  .module("songs")
+  .directive("songShow", function(){
+    return {
+      templateUrl: "/js/songs/_song_show.html",
+      replace: "true",
+      scope: {
+        song: "="
+      },
+      link: function(scope){
+
+      }
+    };
+  });
+})();
